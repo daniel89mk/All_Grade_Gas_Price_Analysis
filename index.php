31feee72e0175aea17f8bcaa804bb42454358d81
daniel89mk <kimberlykang0922@gmail.com>
2022-06-04 22:11:53 -0700

    -

--- index.php
+++ index.php
@@ -49,6 +49,9 @@
           <img src="https://emojipedia-us.s3.amazonaws.com/source/skype/289/backhand-index-pointing-right_1f449.png" style="width:40px;height:40px;">
           <a class="btn btn-Dark" href="https://www.youtube.com/watch?v=uE14fkmyN1U" style="color:white">Our Presentation in Youtube!</a>
         <br></br>
+        <img src="https://image.similarpng.com/very-thumbnail/2020/07/Linkedin-logo-on-transparent-Background-PNG-.png" style="width:40px;height:40px;">
+        <a class="btn btn-Dark" href="https://gas-price-analysis.herokuapp.com/" style="font-size:120%;color:white">OUR WEBSITE!! PLZ CHECKOUT!!</a>
+        <br></br>
           <p2 style="font-size:100%;color: white;">Python, SQL, Tableau, Web Scraping, and Machine Learning</p2>
       </div>
 
